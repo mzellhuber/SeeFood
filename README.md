e9d4b5291574740e1e6925f4374d6ac081e7198c
Meli <melissa.zellhuber@gmail.com>
2018-06-15 11:57:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SeeFood
+
+Silicon Valley copy app that detects if an object is a hotdog or not
+
+#### Made with 💕 and:
+* Swift
+* Xcode
+* Storyboards
+* CoreML
+* InceptionV3 Model
+
+## TODO
+* Improve UI
+* Make it detect other objects
+* Add AppIcon
+* Add LaunchScreen
+
+
+## Author
+
+* ✨✨mzellhuber✨✨ [https://github.com/mzellhuber](https://github.com/mzellhuber)
---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Melissa Zellhuber on 6/15/18.
+//  Copyright © 2018 mzellhuber. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = image
+            guard let ciimage = CIImage(image: image) else{
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image:CIImage)  {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Could not get results from Image")
+            }
+            
+            //print(results)
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog"
+                }else{
+                    self.navigationItem.title = "Not Hotdog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
